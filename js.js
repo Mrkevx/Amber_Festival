eac378923223ca5b0aa59645d45f0d1f90a24a73
Kevin Kristensen <kevi1379@easv365.dk>
2016-05-19 10:05:00 +0200

    Website BITCHES

---
+++ js.js
@@ -0,0 +1,32 @@
+var deadline = 'September 23 2016 12:00:00 GMT+01:00';
+
+function getTimeRemaining(endtime){
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor( (t/1000) % 60);
+    var minutes = Math.floor((t/1000/60) % 60);
+    var hours = Math.floor((t/(1000*60*60)) % 24);
+    var days = Math.floor(t/(1000*60*60*24) );
+    return{
+        'total':t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+
+}
+var daysSpan = clock.querySelector('.days');
+var hoursSpan = clock.querySelector('.hours');
+var minutesSpan = clock.querySelector('.minutes');
+var secondsSpan = clock.querySelector('.seconds');
+
+function updateClock(){
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = t.hours;
+    minutesSpan.innerHTML = t.minutes;
+    secondsSpan.innerHTML = t.seconds
+
+}
+
